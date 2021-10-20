3d6cdb1c4bb39a28e176f800be421bea4cb2cc1c
Leandran <leandranm@outlook.com>
2021-10-20 14:35:24 +0200

    added validations to job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,5 @@
 class Job < ApplicationRecord
+    #validations are the required fields the user must enter when adding a new job
+    validates :title, :company, :url, presence: true
+    validates :url, uniqueness: true
 end
